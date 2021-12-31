ee682fb7aa09d28b7d1b127bc2b824455d1dcc2c
saimoncse19 <saimoncse19@gmail.com>
2021-12-31 12:36:17 +0600

    removed the dummy testcase and used fstring

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -4,11 +4,8 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+
+    pass
 
 
 if __name__ == '__main__':
